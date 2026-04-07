7614335306eb5ef8f414349142e050ac42b1186d
krishpinto <161019503+krishpinto@users.noreply.github.com>
2026-04-07 19:58:11 +0530

    browser logo modified

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,11 +47,11 @@ export const metadata: Metadata = {
   keywords: SITE_INFO.keywords,
   authors: [
     {
-      name: "ncdai",
+      name: "krishpinto",
       url: SITE_INFO.url,
     },
   ],
-  creator: "ncdai",
+  creator: "krishpinto",
   openGraph: {
     siteName: SITE_INFO.name,
     url: "/",
@@ -79,19 +79,10 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "https://assets.chanhdai.com/images/favicon.ico",
-        sizes: "any",
-      },
-      {
-        url: "https://assets.chanhdai.com/images/favicon.svg",
+        url: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 256'><rect x='0' y='0' width='64' height='64'/><rect x='128' y='0' width='64' height='64'/><rect x='0' y='64' width='64' height='64'/><rect x='64' y='64' width='64' height='64'/><rect x='0' y='128' width='64' height='64'/><rect x='64' y='128' width='64' height='64'/><rect x='0' y='192' width='64' height='64'/><rect x='128' y='192' width='64' height='64'/><rect x='256' y='0' width='64' height='64'/><rect x='320' y='0' width='64' height='64'/><rect x='384' y='0' width='64' height='64'/><rect x='256' y='64' width='64' height='64'/><rect x='448' y='64' width='64' height='64'/><rect x='256' y='128' width='64' height='64'/><rect x='320' y='128' width='64' height='64'/><rect x='384' y='128' width='64' height='64'/><rect x='256' y='192' width='64' height='64'/></svg>",
         type: "image/svg+xml",
       },
     ],
-    apple: {
-      url: "https://assets.chanhdai.com/images/apple-touch-icon.png",
-      type: "image/png",
-      sizes: "180x180",
-    },
   },
 }
 
