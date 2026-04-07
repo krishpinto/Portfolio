f364ba254953289b5e99dc9becd044745e2b36ce
krishpinto <161019503+krishpinto@users.noreply.github.com>
2026-04-07 19:22:22 +0530

    Removed the garbage

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -3,10 +3,8 @@ import type { ProfilePage as PageSchema, WithContext } from "schema-dts"
 
 import { About } from "@/features/portfolio/components/about"
 import { Awards } from "@/features/portfolio/components/awards"
-import { Blog } from "@/features/portfolio/components/blog"
 import { Bookmarks } from "@/features/portfolio/components/bookmarks"
 import { Certifications } from "@/features/portfolio/components/certifications"
-import { Components } from "@/features/portfolio/components/components"
 import { Experiences } from "@/features/portfolio/components/experiences"
 import { GitHubContributions } from "@/features/portfolio/components/github-contributions"
 import { Overview } from "@/features/portfolio/components/overview"
@@ -14,7 +12,6 @@ import { ProfileCover } from "@/features/portfolio/components/profile-cover"
 import { ProfileHeader } from "@/features/portfolio/components/profile-header"
 import { Projects } from "@/features/portfolio/components/projects"
 import { SocialLinks } from "@/features/portfolio/components/social-links"
-import { Sponsors } from "@/features/portfolio/components/sponsors"
 import { TechStack } from "@/features/portfolio/components/tech-stack"
 import { USER } from "@/features/portfolio/data/user"
 import { cn } from "@/lib/utils"
@@ -50,16 +47,7 @@ export default function Page() {
         <GitHubContributions />
         <Separator />
 
-        <Sponsors />
-        <Separator />
-
         <TechStack />
-        <Separator />
-
-        <Components />
-        <Separator />
-
-        <Blog />
         <Separator />
 
         <Experiences />
